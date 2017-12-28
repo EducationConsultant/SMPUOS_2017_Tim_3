df8178be2b1cd27359b82f1d8c8c0d91af6394f0
SvetlanaDjuric <cdjuric963@gmail.com>
2017-12-28 20:38:01 +0100

    Promenjena skripta za rezervaciju.

--- Implementacija/rezervacija/src/main/resources/import.sql
+++ Implementacija/rezervacija/src/main/resources/import.sql
@@ -1,12 +1,12 @@
-INSERT INTO `db_rezervacija`.`projekcija`
-(`projekcija_id`,`broj_aktivnih_rezervacija`,`broj_otkazanih_rezervacija`,`datum_projekcije`,
-`id_bioskopa`,`id_filma`,`id_sale`,`naziv_bioskopa`,`naziv_filma`,`oznaka_sale`)
+insert into projekcija
+('projekcija_id','broj_aktivnih_rezervacija','broj_otkazanih_rezervacija','datum_projekcije',
+'id_bioskopa','id_filma','id_sale','naziv_bioskopa','naziv_filma','oznaka_sale')
 VALUES
 (1,0,0,'2012-01-01',1,1,1,'Bioskop za mlade','Ferdinand - Sinh','Sala 1');
 
 
-INSERT INTO `db_rezervacija`.`rezervacija`
-(`rezervacija_id`,`broj_reda_sedista`,`broj_sedista`,`datum_isteka_rezervacije`,`datum_rezervacije`,
-`id_korisnika`,`tip`,`projekcija_id`)
+insert into rezervacija
+('rezervacija_id','broj_reda_sedista','broj_sedista','datum_isteka_rezervacije','datum_rezervacije',
+'id_korisnika','tip','projekcija_id')
 VALUES
-(1,1,1,'2012-01-01','2012-01-05', 1,'AKTIVNA', 1);
+(1,1,1,'2012-01-01',STR_TO_DATE('12-01-2016 00:00:00','%m-%d-%Y %H:%i:%s'), 1,'AKTIVNA', 1);
