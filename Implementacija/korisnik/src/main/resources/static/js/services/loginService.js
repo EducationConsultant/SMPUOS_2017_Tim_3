cfac681602f6f988c4d5ef68964e9ff31040421d
markovjestica <m.vjestica94@gmail.com>
2017-12-28 18:29:03 +0100

    Omogućeno registrovanje korisnika #19

--- Implementacija/korisnik/src/main/resources/static/js/services/loginService.js
+++ Implementacija/korisnik/src/main/resources/static/js/services/loginService.js
@@ -5,6 +5,14 @@ angular.module('korisnikApp.LoginService', [])
         factory.login = function (tempUser) {
             return $http.put('/korisnik-service/korisnik/login', tempUser);
         }
+        
+        factory.logout = function (tempUser) {
+            return $http.put('/korisnik-service/korisnik/logout', tempUser);
+        }
+        
+        factory.registrovanje = function (tempUser) {
+            return $http.post('/korisnik-service/korisnik/registracija', tempUser);
+        }
 
         return factory;
     });
