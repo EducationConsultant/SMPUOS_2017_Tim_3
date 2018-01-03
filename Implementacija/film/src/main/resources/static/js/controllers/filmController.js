b1065773283b9aa509d6eab42cdbff6087340963
biljanaandjelic <abiljana94@gmail.com>
2018-01-03 02:09:58 +0100

    Dodate su metode koje omogucavaju dobavljanje jezika, kategorije, reditelja i gumaca sa backenda na frontend.

--- Implementacija/film/src/main/resources/static/js/controllers/filmController.js
+++ Implementacija/film/src/main/resources/static/js/controllers/filmController.js
@@ -15,12 +15,53 @@ angular.module('filmApp.FilmController',[])
 	$scope.obrisiFilm=function(id){
 		alert("Brisanje filma "+ id);
 		FilmoviService.brisanje(id);
-	}
+	};
+	
+	$scope.getJezici=function(){
+		FilmoviService.jezici()
+		.success(
+			function(data) {
+				$scope.listaJezika = data;
+		})
+	};
+	
+	
+	$scope.getKategorije=function(){
+		
+		FilmoviService.kategorije()
+		.success(
+			function(data) {
+				$scope.listaKategorija = data;
+		})
+	};
+	
+	$scope.getReditelji=function(){
+		FilmoviService.reditelji()
+		.success(
+			function(data) {
+				$scope.listaReditelja = data;
+		})
+	};
+	
+	$scope.getGlumci=function(){
+		FilmoviService.glumci()
+		.success(
+			function(data) {
+				$scope.listaGlumaca = data.content;
+		})
+	};
 	
 	isAdmin=function(){
 		$scope.isAdmin= $localStorage.tip =='ADMIN';
 		var value=$scope.isAdmin;
 	};
+	
+	$scope.init=function(){
+		$scope.getGlumci();
+		$scope.getKategorije();
+		$scope.getReditelji();
+		$scope.getJezici();
+	}
 	
 	isAdmin();
 	
