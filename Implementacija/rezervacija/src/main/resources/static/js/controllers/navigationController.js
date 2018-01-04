6bf86968b16b4de9bc4813eaf85962826abd3a2d
AndrijanaStefanovic <anja.stef@gmail.com>
2018-01-04 20:19:40 +0100

    Izmene pocetne strane za rezervacije #18

--- Implementacija/rezervacija/src/main/resources/static/js/controllers/navigationController.js
+++ Implementacija/rezervacija/src/main/resources/static/js/controllers/navigationController.js
@@ -3,8 +3,10 @@ angular.module('rezervacijaApp.NavigationController', []).controller(
 		function($scope, $location, $rootScope, $mdDialog, RezervacijaService,
 				$localStorage, $mdToast) {
 
-			if ($localStorage.curNav == null)
-				$scope.currentNavItem = 'Pocetna';
+			if ($localStorage.curNav == null){
+
+		        	$scope.currentNavItem = 'Pocetna';
+			}
 			else
 				$scope.currentNavItem = $localStorage.curNav;
 
@@ -12,7 +14,12 @@ angular.module('rezervacijaApp.NavigationController', []).controller(
 				$localStorage.curNav = data;
 			}
 
-			
-			
+			if($localStorage.tip == 'REGKORISNIK'){
+	            $scope.showRegistrovan = true;
+	            $scope.showAdministrator = false;
+	        }else if($localStorage.tip == 'ADMIN'){
+	            $scope.showRegistrovan = false;
+	            $scope.showAdministrator = true;
+	        }
 			
 		});
